a03858659201af121d8636c08b78de8b8dacc164
Kate <you@example.com>
2020-10-28 17:45:44 +0300

    adding models

--- tutorialproject/models.py
+++ tutorialproject/models.py
@@ -5,10 +5,11 @@ from django.db import models
 class Tutorial(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    iimage = models.ImageField(upload_to='images/', blank=True)
-    content=models
+    image = models.ImageField(upload_to='images/', blank=True)
+    content=models.TextField(max_length=255)
     pub_date = models.DateTimeField(auto_now_add=True)
-    Author = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
-    author_profile = models.ForeignKey(Profile,on_delete=models.CASCADE, blank=True, default='1')
-    link = models.URLField()
-    country = CountryField(blank_label='(select country)', default='Kenya')
+    updated_date=models.DateTimeField(auto_now_add=True)
+    Author = models.CharField(max_length=50)
+    Published=models.BooleanField()
+    
+    
