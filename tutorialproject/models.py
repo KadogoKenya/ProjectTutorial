c9b7e193af85cd4dec69a69e992b5739f506a13d
Kate <you@example.com>
2020-10-29 14:59:26 +0300

    model methods added

--- tutorialproject/models.py
+++ tutorialproject/models.py
@@ -5,14 +5,21 @@ from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
 class Tutorial(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     image = image = CloudinaryField('images/', blank=True)
     content=models.TextField(max_length=255)
     pub_date = models.DateTimeField(auto_now_add=True)
-    updated_date=models.DateTimeField(auto_now_add=True)
-    Author = models.CharField(max_length=50)
+    mod_date=models.DateTimeField(auto_now_add=True)
+    authors = models.ManyToManyField(Author)
     Published=models.BooleanField()
     Unpublished=models.BooleanField(default=False)
 
@@ -25,6 +32,12 @@ class Tutorial(models.Model):
     def get_all_tutorials(cls):
         images = cls.objects.all()
         return images
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):        
+        return reverse('index')
     
 
 
