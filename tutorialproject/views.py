8ad581d47237b08229ed750f5e1a2a0957417336
Kate <you@example.com>
2020-10-29 15:52:14 +0300

    creating views

--- tutorialproject/views.py
+++ tutorialproject/views.py
@@ -4,6 +4,15 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from .models import Tutorial
+from django.views.generic import  ListView,DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.shortcuts import get_object_or_404
+from users.models import Profile
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 def index(request):
     return render(request, 'tutorial/index.html')
