6527d1ce5186c6393fe13a67f099d9dcf0334261
Kate <you@example.com>
2020-10-29 23:10:32 +0300

    search form added

--- tutorialproject/urls.py
+++ tutorialproject/urls.py
@@ -25,17 +25,8 @@ urlpatterns = [
     path('tutorial/new/', TutorialCreateView.as_view(), name='tutorial-create'),
     path('tutorial/<int:pk>/update/', TutorialUpdateView.as_view(), name='tutorial-update'),
     path('tutorial/<int:pk>/delete/', TutorialDeleteView.as_view(), name='tutorial-delete'),
+    url(r'^search/', views.search_results, name='search_results'),
 
-    # path('project/<int:pk>/', ProjectDetailView.as_view(), name='project-detail'),
-    # url(r'^new/tutorial$', views.new_tutorial, name='new-tutorial'),
-    # url(r'^tutorials/(\d+)',views.tutorials,name='tutorials'),
-
-    # url('',views.index, name='index'),
-    # path('hello/', views.HelloView.as_view(), name='hello'),
-
-    # path(r'^$', TutorialListView.as_view(), name='tutorialpoint'),
-    # path('tutorial/new/', TutorialCreateView.as_view(), name='tutorial-create'),
-    
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     
