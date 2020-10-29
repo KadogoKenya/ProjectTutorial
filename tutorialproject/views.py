39bd0b27b8617c6e6ffc0287f31c8bd279ffda53
Kate <you@example.com>
2020-10-29 23:23:52 +0300

    search template

--- tutorialproject/views.py
+++ tutorialproject/views.py
@@ -84,13 +84,13 @@ class TutorialDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
 
 def search_results(request):
 
-    if 'article' in request.GET and request.GET["article"]:
-        search_term = request.GET.get("article")
-        searched_articles = Article.search_by_title(search_term)
+    if 'tutorial' in request.GET and request.GET["tutorial"]:
+        search_term = request.GET.get("tutorial")
+        searched_tutorials = Tutorial.search_by_title(search_term)
         message = f"{search_term}"
 
-        return render(request, 'all-news/search.html',{"message":message,"articles": searched_articles})
+        return render(request, 'tutorial/search.html',{"message":message,"tutorial": searched_tutorial})
 
     else:
         message = "You haven't searched for any term"
-        return render(request, 'all-news/search.html',{"message":message})
+        return render(request, 'tutorial/search.html',{"message":message})
